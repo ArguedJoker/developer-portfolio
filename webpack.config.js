37977756f0e6947f305fe6b77c678aa77615545a
Rohan <rohanpschauhan@gmail.com>
2024-12-28 00:01:36 +0000

    added Project configuration and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico'
+        }),
+        new Dotenv({
+            systemvars: true,
+            defaults: false
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { 
+                    from: 'src/css',
+                    to: 'css'
+                }
+            ]
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+            publicPath: '/'
+        },
+        historyApiFallback: true,
+        port: 3001,
+        hot: true,
+        compress: true
+    }
+};
